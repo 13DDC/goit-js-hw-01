cf5e026420a7bcab8952ea4ef3299baed2dbe3fe
13DDC <150695013+13DDC@users.noreply.github.com>
2024-03-16 22:24:05 +0200

    hm 1 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,11 @@
 function getElementWidth(content, padding, border) {
-  content = Number.parseFloat(content);
-  padding = Number.parseFloat(padding);
-  border = Number.parseFloat(border);
-  let boxSizing = content + padding * 2 + border * 2;
+  contentWidth = Number.parseFloat(content);
+  paddingWidth = Number.parseFloat(padding);
+  borderWidth = Number.parseFloat(border);
+  let boxSizing = contentWidth + paddingWidth * 2 + borderWidth * 2;
   return boxSizing;
 }
 
-getElementWidth('50px', '8px', '4px');
-getElementWidth('60px', '12px', '8.5px');
-getElementWidth('200px', '0px', '0px');
 console.log(getElementWidth('50px', '8px', '4px'));
 console.log(getElementWidth('60px', '12px', '8.5px'));
 console.log(getElementWidth('200px', '0px', '0px'));
